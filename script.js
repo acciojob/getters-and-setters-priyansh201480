0e33e10693df514c88600bf6a35f06167f328c07
Priyansh Agarwal <140069604+priyansh201480@users.noreply.github.com>
2024-10-10 20:42:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-//complete this code
-class Person {}
+// Define the Person class
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(age) {
+    this._age = age;
+  }
+}
+
+// Define the Student subclass
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+// Define the Teacher subclass
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
